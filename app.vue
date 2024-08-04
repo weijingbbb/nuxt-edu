046a374d3e876d51fcd81693efcf3ca5af605d5d
weijingbbb <1047129443@qq.com>
2024-08-04 20:56:51 +0800

    6 - 创建 错误页

--- app.vue
+++ app.vue
@@ -3,3 +3,8 @@
     <button class="text-6xl">按钮</button>
   </div>
 </template>
+
+<script setup>
+throwError('一不小心就出错啦')
+
+</script>
---
+++ error.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app';
+
+const props = defineProps({
+    error: Object as () => NuxtError
+})
+
+const handleError = () => clearError({ redirect: '/' })
+</script>
+
+<template>
+    <div class="mt-20">
+    <n-result status="500" title="500 服务器错误" description="服务器出错可能说明该雇更多程序员了">
+        <template #footer>
+            <n-button @click="handleError">回到首页</n-button>
+        </template>
+    </n-result>
+</div>
+</template>
